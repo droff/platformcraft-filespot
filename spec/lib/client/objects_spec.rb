826f8cc632b553c7fd701c1b5e39e856eae3060a
droff <adroff@gmail.com>
2016-04-16 23:24:12 +0300

    stub refactor

--- spec/lib/client/objects_spec.rb
+++ spec/lib/client/objects_spec.rb
@@ -3,8 +3,7 @@ require 'spec_helper'
 describe Filespot::Client do
   describe 'Objects' do
     it '.get_objects' do
-      uri = 'http://api.platformcraft.ru/1/objects?apiuserid=test&hash=46c5500379d2c09c6f8972c7fd79c27fd7ebf0dd0ab47833a034fd613a4f4d93&timestamp=1450342665'
-      stub_get(uri, 'objects.json')
+      stub_get('/objects', 'objects.json')
       objects = Filespot::Client.get_objects
       expect(objects.count).to eq 2
     end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,9 +22,10 @@ RSpec.configure do |config|
   end
 end
 
-def stub_get(uri, fixture_name)
+def stub_get(api_method, fixture_name)
   encoding = 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3'
   agent = 'Faraday v0.9.2'
+  uri = "http://api.platformcraft.ru/1#{api_method}?apiuserid=test&hash=46c5500379d2c09c6f8972c7fd79c27fd7ebf0dd0ab47833a034fd613a4f4d93&timestamp=1450342665"
   stub_request(:get, uri).with(headers: {'Accept'=>'*/*', 'Accept-Encoding'=> encoding, 'User-Agent'=> agent}).to_return(status: 200, body: fixture(fixture_name), headers: {})
 end
 
